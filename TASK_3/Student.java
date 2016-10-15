8440a78efe32f4d78f87547df95d9a92579ea403
Aksultan <aksultan919@gmail.com>
2016-10-15 22:09:31 +0600

    d

--- TASK_3/Student.java
+++ TASK_3/Student.java
@@ -1,25 +1,68 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
+public class Student implements Comparable<Student>{
+    public Student(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    public Student(int id, String name, String surname, int age,
+                   String group, String[] grades, int debt) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.group = group;
+        this.grades = Arrays.stream(grades).mapToInt(Integer::parseInt).toArray();
+        this.debt=debt;
+    }
+    String name,surname,group;
+    int age,debt;
+    int grades[];
+    int id;
+    public int compareTo(Student o) {
+        if (this.id > o.id)
+            return 1;
+        else if (this.id == o.id)
+            return 0;
+        else
+            return -1;
+    }
+}
+class Averages implements Comparator {
+    public int compare( Object o1, Object o2 ){
 
-public class Student{
-	public Student(String name, String surname) {
-		this.name = name;
-		this.surname = surname;
-	}
-	public Student(int id, String name, String surname, int age,
-			String group, String[] grades, int debt) {
-		this.id = id;
-		this.name = name;
-		this.surname = surname;
-		this.age = age;
-		this.group = group;
-		this.grades = Arrays.stream(grades).mapToInt(Integer::parseInt).toArray();
-	}
-	String name,surname,group;
-	int age,debt;
-	int grades[];
-	int id;
-	public String toString(){
-		return id+" "+name+" "+surname+" "+age+" "+group+" "+Arrays.toString(grades);
-	}
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int i=0;i<((Student)o1).grades.length;i++){
+            sum1+=((Student)o1).grades[i];
+            sum2+=((Student)o2).grades[i];
+        }
+        int avg1=sum1/((Student)o1).grades.length;
+        int avg2=sum2/((Student)o2).grades.length;
+
+        if ( avg1 > avg2 ){ return 1; }
+        else if ( avg1 < avg2 ){ return -1; }
+        return 0;
+    }
+}
+class Debts implements Comparator {
+    public int compare( Object o1, Object o2 ){
+
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int i=0;i<((Student)o1).grades.length;i++){
+            sum1+=((Student)o1).grades[i];
+            sum2+=((Student)o2).grades[i];
+        }
+        int avg1=sum1/((Student)o1).grades.length;
+        int avg2=sum2/((Student)o2).grades.length;
+
+        int debt1=((Student)o1).debt;
+        int debt2=((Student)o2).debt;
+
+        if ( avg1 > avg2 ){ return 1; }
+        else if ( avg1 < avg2 ){ return -1; }
+        return 0;
+    }
 }
--- TASK_3/StudentViewerTask.java
+++ TASK_3/StudentViewerTask.java
@@ -3,43 +3,71 @@ import java.nio.file.Files;
 import java.nio.file.*;
 import java.util.*;
 public class StudentViewerTask {
-	public static void main(String args[]){
-		Scanner in = new Scanner(System.in);
-		HashMap<String,ArrayList<Student>> groups = new HashMap<>();
-		ArrayList<Student> students = new ArrayList<>();
-		loadValues(groups,students);
-		/* This lines should output students ordered by id
-		 * Uncomment following line	 */
-		//Collections.sort(groups.get("A03"));
-		System.out.println((groups.get("A03")));
-		/* Output students sorted by their average */
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        HashMap<String,ArrayList<Student>> groups = new HashMap<>();
+        ArrayList<Student> students = new ArrayList<>();
+        loadValues(groups,students);
 
-		/* Output students who doesn't have debt and has average above 80 points */
+        Collections.sort(groups.get("A03"));
+       // System.out.println(groups.get("A03"));
 
-		/* Your code should return ArrayList<String>
-		 * that contains name and surname in "Mr. J. Bezos" format */
+        Collections.sort(groups.get("A03"),new Averages());
+        // System.out.println(groups.get("A03"));
+        ArrayList<String> debts = new ArrayList<>();
+        for (String l: groups.keySet()) {
+            for (int i = 0; i < groups.get(l).size(); i++) {
+                int summ = 0;
+                for (int y = 0; y < groups.get(l).get(i).grades.length; y++) {
+                    summ += groups.get(l).get(i).grades[y];
+                }
+                if (summ>240 & groups.get(l).get(i).debt == 0) {
+                    debts.add("Mr. " + groups.get(l).get(i).name.substring(0, 1) + ". " + groups.get(l).get(i).surname);
+                }
+            }
+        }
+        System.out.println(debts);
 
-		/* Find if student with specified name and surname present in list */
-		String name = in.nextLine();
-		String surname = in.nextLine();
-		Student s = new Student(name,surname);
+        String name = in.nextLine();
+        String surname = in.nextLine();
+        Student s = new Student(name,surname);
+        System.out.println(s.name+" "+s.surname);
+        for (String l: groups.keySet()) {
+            for(int i=0;i<groups.get(l).size();i++){
+                if(groups.get(l).get(i).name.equals(s.name) & groups.get(l).get(i).surname.equals(s.surname)){
+                    System.out.println("Exist");
+                }
+            }
+        }
+		int count=0;
+        for (String l: groups.keySet()) {
+            for (int x = 0; x < groups.get(l).size(); x++) {
+                int summ = 0;
+                for (int y = 0; y < groups.get(l).get(x).grades.length; y++) {
+                    summ += groups.get(l).get(x).grades[y];
+                }
+                if (summ>150) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
 
-		/* Count number of students from each group that got more than 50 points in average*/
+        in.close();
+    }
+    public static void loadValues(HashMap<String,ArrayList<Student>> maps,ArrayList<Student> students){
+        try{
+            List<String> lines = Files.readAllLines(Paths.get(".", "students.txt"),StandardCharsets.UTF_8);
+            lines.forEach((line)->{
+                String parts[] = line.split(" ");
+                Student s = new Student(Integer.parseInt(parts[0]),parts[1],parts[2],Integer.parseInt(parts[3]),parts[4],parts[5].split(";"),Integer.parseInt(parts[6]));
+                students.add(s);
+                maps.putIfAbsent(s.group, new ArrayList<Student>());
+                maps.get(s.group).add(s);
+            });
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
-		in.close();
-	}
-	public static void loadValues(HashMap<String,ArrayList<Student>> maps,ArrayList<Student> students){
-		try{
-			List<String> lines = Files.readAllLines(Paths.get(".", "students.txt"),StandardCharsets.UTF_8);
-			lines.forEach((line)->{
-				String parts[] = line.split(" ");
-				Student s = new Student(Integer.parseInt(parts[0]),parts[1],parts[2],Integer.parseInt(parts[3]),parts[4],parts[5].split(";"),Integer.parseInt(parts[6]));
-				students.add(s);
-				maps.putIfAbsent(s.group, new ArrayList<Student>());
-				maps.get(s.group).add(s);
-			});
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
 }
